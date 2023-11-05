abd329ff61df0b01ce5615843856e0346dd8fe3a
suneel-nadipalli <140884446+suneel-nadipalli@users.noreply.github.com>
2023-11-05 19:50:37 +0530

    Delete pipeline directory

--- pipeline/transcribe.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-def transcribe_audio(audio_path):
-    audio_file = open(audio_path, "rb")
-
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-    return (dict(transcript)['text'])
